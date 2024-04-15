1a0b08ea352687d0f762f9461cd832545d7c42b5
LapisBerry <kittiphopkhankaew@gmail.com>
2024-04-16 05:37:59 +0700

    fix: contact page & import font Inter

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -4,17 +4,19 @@ export default function contact() {
     return (
         <main className="text-center m-[3%] item-center justify-center">
             <div className="text-bold text-3xl">Team</div>
-            <div className="m-20 ml-[20%] flex flex-row wrap space-around ">
-                <div className="mx-10 w-1/5">
-                    <Image src={'/img/yurill.jpg'} alt="rawit" width={400} height={400} className="" />
+
+            {/* To the next guy who wants to edit this: USE GRID*/}
+            <div className="flex flex-row justify-center wrap space-around">
+                <div className="flex flex-col mx-10 w-1/5 items-center">
+                    <Image src={'/img/yurill.jpg'} alt="rawit" width={400} height={400} className="rounded-full" />
                     <div className="my-4 text-xl">Rawit Pholngam</div>
                 </div>
-                <div className="mx-10 w-1/5">
-                    <Image src={'/img/useless.png'} alt="rawit" width={400} height={400} className="" />
+                <div className="flex flex-col mx-10 w-1/5 items-center">
+                    <Image src={'/img/useless.png'} alt="nithi" width={400} height={400} className="rounded-full" />
                     <div className="my-4 text-xl">Nithi Panutat</div>
                 </div>
-                <div className="mx-10 w-1/5">
-                    <Image src={'/img/reo.png'} alt="rawit" width={400} height={400} className="" />
+                <div className="flex flex-col mx-10 w-1/5 items-center">
+                    <Image src={'/img/reo.png'} alt="thamvarut" width={400} height={400} className="rounded-full" />
                     <div className="my-4 text-xl">Thamvarut Wannachetisara</div>
                 </div>
             </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
     ],
     theme: {
         extend: {
-            colors: {
+            colors: { // example: className="text-gray-slate-dark"
                 "paper": "#F9F2ED",
                 "gray-slate-dark": "#304159", // tailwind: text-slate-800
                 "orange-sweet": "#FF7E67", // tailwind: text-orange-500
             },
         },
+        fontFamily: { // example: className="font-Inter"
+            "Inter": ["Inter", "sans-serif"],
+        }
     },
     plugins: [],
 };
