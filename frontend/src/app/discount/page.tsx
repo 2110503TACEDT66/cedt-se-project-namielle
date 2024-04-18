b572dc7b59b2b3b45530eba8be74e6e18d0fe9b9
Copy <masterkcopyk@gmail.com>
2024-04-18 12:42:41 +0700

    remain code to make real discount

--- frontend/src/app/discount/page.tsx
+++ frontend/src/app/discount/page.tsx
@@ -5,7 +5,7 @@ export default function discounts() {
     const discounts = getDiscounts()
     return (
         <main className="text-center p-5">
-            <h1 className="text-xl font-medium mt-12 text-black">Today Deal !!!</h1>
+            <h1 className="text-orange-500 font-sans font-black text-8xl mb-8 center">Today Deal !!!</h1>
             <DiscountCatalog discountJson={discounts}/>
         </main>
     )
