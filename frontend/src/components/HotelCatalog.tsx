ced291287db380d4b9c258759e8e5ec76c7edf77
LapisBerry <kittiphopkhankaew@gmail.com>
2024-04-25 15:07:47 +0700

    style: HotelCatalog response to browser theme EXCLUDE RecommendBanner

--- frontend/src/components/HotelCatalog.tsx
+++ frontend/src/components/HotelCatalog.tsx
@@ -26,26 +26,34 @@ export default function HotelCatalog({ hotelJson }: { hotelJson: any }) {
 
     return (
         <div className="justify-center item-center">
-            <div className="container mx-auto my-8 p-4 rounded-lg shadow-md block">
+            <div className="container mx-auto my-8 p-4 rounded-lg shadow-md block bg-paper-yellow dark:bg-midnight-dark">
                 <div className="flex flex-row justify-center">
                     <div className="m-1 w-[60%]">
-                        <h2 className="text-lg font-semibold text-gray-800">Search Your Hotels</h2>
+                        <h2 className="text-lg font-semibold text-gray-sweet dark:text-white-grayish">Search Your Hotels</h2>
                         <input
                             type="text"
                             id="search"
                             name="search"
                             placeholder="Search..."
                             onChange={(e) => setSearch(e.target.value)}
-                            className="input input-bordered text-l p-2 mr-[2%] w-[100%] h-[65%] border-solid border border-gray-400 rounded-md bg-white"
+                            className="input input-bordered text-lg p-2 mr-[2%] w-[100%] h-[65%] border-solid border border-gray-400 rounded-md bg-paper-more-yellow dark:bg-midnight-blue"
                         />
                     </div>
                     <div className="m-1 w-[10%]">
-                        <h2 className="text-lg font-semibold text-gray-800">Persons</h2>
-                        <input type="number" id="persons" name="persons" placeholder="Persons" min={0} className="input input-bordered text-l p-2 w-[100%] h-[65%] border-solid border border-gray-400 rounded-md bg-white" onChange={(e) => setPersons(Number(e.target.value))} />
+                        <h2 className="text-lg font-semibold text-gray-sweet dark:text-white-grayish">Persons</h2>
+                        <input
+                            type="number"
+                            id="persons"
+                            name="persons"
+                            placeholder="Persons"
+                            min={0}
+                            onChange={(e) => setPersons(Number(e.target.value))}
+                            className="input input-bordered text-lg p-2 w-[100%] h-[65%] border-solid border border-gray-400 rounded-md bg-paper-more-yellow dark:bg-midnight-blue"
+                        />
                     </div>
                 </div>
                 <div className="text-center">
-                    <h1 className="text-lg pt-2 font-semibold text-center font-black ">
+                    <h1 className="text-lg pt-2 text-center font-black text-purple-dark dark:text-white-grayish">
                         {hotelData?.data.filter((hotelItem: any) => {
                             return (search.toLowerCase() === '') ? hotelItem : hotelItem.name.toLowerCase().includes(search.toLowerCase()) || hotelItem.city.toLowerCase().includes(search.toLowerCase())
                         }).filter((hotelItem: any) => {
