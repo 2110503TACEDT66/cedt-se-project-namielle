4a388c2cae606c929a16012c740a64719828b621
webbalaka <webbalaka@gmail.com>
2024-04-26 21:20:13 +0700

    fix: labels to medals

--- frontend/src/components/RecommendBanner.tsx
+++ frontend/src/components/RecommendBanner.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import { Button } from "@mui/material"
-import Image from "next/image"
+import { Button } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import getHotels from "@/libs/getHotels";
@@ -9,7 +9,7 @@ import RecommendCard from "./RecommendCard";
 
 export default function RecommendBanner() {
     const [hotelData, setHotelData] = useState<any>();
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState("");
     const [sortedArray, setSortedArray] = useState<any[]>([]);
 
     // Add state for sortedArray
@@ -22,10 +22,10 @@ export default function RecommendBanner() {
                 //Calculate Rank
                 const sorted = result?.data.sort((n1: any, n2: any) => {
                     if (n1.priority > n2.priority) {
-                        return -1
+                        return -1;
                     }
                     if (n1.priority < n2.priority) {
-                        return 1
+                        return 1;
                     }
 
                     if (n1.bookCount > n2.bookCount) {
@@ -40,63 +40,60 @@ export default function RecommendBanner() {
                 });
 
                 setSortedArray(sorted);
+                console.log(sorted);
             } catch (err) {
                 console.error(err);
             }
         };
 
         fetchUserData();
-    }, [])
+    }, []);
 
     // Now it's safe to log sortedArray
     useEffect(() => {
         console.log(sortedArray);
-    }, [sortedArray])
+    }, [sortedArray]);
 
     return (
-        <div className="flex flex-row p-1 h-[25vh] bg-stone-400 ">
-            <div className="w-[20%] px-5">
-                <p className="font-semibold font-serif text-3xl text-orange-300 leading-loose">Recommended <span className="text-white bold">For you</span></p>
+        <div className="flex flex-row items-center content-between gap-[5%] h-[30vh] bg-stone-400 ">
+            <div className="w-fit px-5 flex flex-col">
+                <p className="font-semibold font-serif text-3xl text-orange-300 leading-loose">
+                    Recommended
+                </p>
+                <span className="text-white bold text-lg">For you</span>
             </div>
-            <div className="w-[1%] h-[90%] bg-white text-black flex items-center justify-center mt-[0.5%]">
-                T O P 1
-            </div>
-            {<div className="w-1/5 pr-6 h-[100%] relative mr-[3%] mt-[0.5%]">
-                {sortedArray && sortedArray.length > 0 && (
-                    <Link key={sortedArray[0].name} href={`/hotel/${sortedArray[0].id}`}>
-                        <RecommendCard
-                            hotelName={sortedArray[0].name}
-                            imgSrc={`/img/${sortedArray[0].file}`}
-                        />
-                    </Link>
-                )}
-            </div>}
-            <div className="w-[1%] h-[90%] bg-white text-black flex items-center justify-center mt-[0.5%]">
-                T O P 2
-            </div>
-            {<div className="w-1/5 pr-6 h-[100%] relative mr-[3%] mt-[0.5%]">
-                {sortedArray && sortedArray.length > 0 && (
-                    <Link key={sortedArray[1].name} href={`/hotel/${sortedArray[1].id}`}>
-                        <RecommendCard
-                            hotelName={sortedArray[1].name}
-                            imgSrc={`/img/${sortedArray[1].file}`}
-                        />
-                    </Link>
-                )}
-            </div>}
-            <div className="w-[1%] h-[90%] bg-white text-black flex items-center justify-center mt-[0.5%]">
-                T O P 3
-            </div>
-            {<div className="w-1/5 pr-6 h-[100%] relative mr-[3%] mt-[0.5%]">
-                {sortedArray && sortedArray.length > 0 && (
-                    <Link key={sortedArray[2].name} href={`/hotel/${sortedArray[2].id}`}>
-                        <RecommendCard
-                            hotelName={sortedArray[2].name}
-                            imgSrc={`/img/${sortedArray[2].file}`}
-                        />
-                    </Link>
-                )}
-            </div>}
+            {[1, 2, 3].map((count) => 
+                (
+                    <div className="Card1 w-[20%] h-[90%] flex items-center">
+                        {/* <div className="w-[5%] h-[100%] bg-white text-pink-sweet font-bold flex items-center flex-col justify-center">
+                            <div>T</div>
+                            <div>O</div>
+                            <div>P</div>
+                            <div>{count}</div>
+                        </div> */}
+                        {
+                            <div className="w-full h-[100%] relative flex items-center flex-col">
+                                {sortedArray && sortedArray.length > 0 && (
+                                    <Link
+                                        key={sortedArray[count].name}
+                                        href={`/hotel/${sortedArray[count].id}`}
+                                        className="w-full h-full"
+                                    >
+                                        <RecommendCard
+                                            hotelName={sortedArray[count].name}
+                                            hotelCity={sortedArray[count].city}
+                                            hotelPrice={sortedArray[count].price}
+                                            rankImage={`/img/rank/rank${count}.png`}
+                                            imgSrc={`/img/${sortedArray[count].file}`}
+                                        />
+                                    </Link>
+                                )}
+                            </div>
+                        }
+                    </div>
+                ))
+
+            }
         </div>
-    )
+    );
 }
--- frontend/src/components/RecommendCard.tsx
+++ frontend/src/components/RecommendCard.tsx
@@ -3,14 +3,44 @@ import CardTemplate from "./CardTemplate";
 import getHotels from "@/libs/getHotels";
 import getHotel from "@/libs/getHotel";
 
-export default function RecommendCard({ hotelName, imgSrc }: { hotelName: string, imgSrc: string }) {
+export default function RecommendCard({ hotelName, hotelCity, hotelPrice, rankImage,  imgSrc }: { hotelName: string, hotelCity: string, hotelPrice: number, rankImage: string, imgSrc: string }) {
 
     // const randPrice = Math.floor(Math.random() * (10000 - 100 + 1)) + 100;
-
+    const formatter = new Intl.NumberFormat("th-TH", {
+        style: "currency",
+        currency: "THB",
+    });
 
     return (
-        <div className="w-[100%] h-[90%] relative">
-            <Image src={imgSrc} alt={hotelName} fill={true} className="object-cover" />
+        <div className="w-[100%] h-[100%] relative">
+            <div className="absolute top-0 left-0 z-10">
+                <Image
+                    src={rankImage}
+                    width={75}
+                    height={75}
+                    alt="rank image"
+                ></Image>
+            </div>
+            <div className="w-[100%] h-[70%] relative rounded-xl">
+                <Image src={imgSrc} alt={hotelName} fill={true} className="object-cover rounded-t-md"/>
+            </div>
+            <div className="w-full h-[30%] bg-white p-1">
+                <div className="font-bold text-lg pl-1">
+                    {hotelName}
+                </div>
+                <div className="flex">
+                    <Image
+                        src={"/img/location.png"}
+                        width={20}
+                        height={20}
+                        alt="location image"
+                    ></Image>
+                    {hotelCity}
+                </div>
+                <div className="absolute bottom-0 right-0 p-1 text-2xl text-orange-500">
+                    {formatter.format(hotelPrice).replace('฿', 'THB ')}.-
+                </div>
+            </div>
         </div>
     )
 }
