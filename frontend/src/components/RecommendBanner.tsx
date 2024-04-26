951318d6be2e741311ba84a7b6770d273a2d955b
webbalaka <webbalaka@gmail.com>
2024-04-26 23:04:05 +0700

    add: rating in recommend

--- frontend/src/components/RecommendBanner.tsx
+++ frontend/src/components/RecommendBanner.tsx
@@ -8,7 +8,6 @@ import getHotels from "@/libs/getHotels";
 import RecommendCard from "./RecommendCard";
 
 export default function RecommendBanner() {
-    const [hotelData, setHotelData] = useState<any>();
     const [search, setSearch] = useState("");
     const [sortedArray, setSortedArray] = useState<any[]>([]);
 
@@ -18,7 +17,6 @@ export default function RecommendBanner() {
         const fetchUserData = async () => {
             try {
                 const result = await getHotels();
-                setHotelData(result);
                 //Calculate Rank
                 const sorted = result?.data.sort((n1: any, n2: any) => {
                     if (n1.priority > n2.priority) {
@@ -40,7 +38,6 @@ export default function RecommendBanner() {
                 });
 
                 setSortedArray(sorted);
-                console.log(sorted);
             } catch (err) {
                 console.error(err);
             }
@@ -55,45 +52,48 @@ export default function RecommendBanner() {
     }, [sortedArray]);
 
     return (
-        <div className="flex flex-row items-center content-between gap-[5%] h-[30vh] bg-stone-400 ">
+        <div className="flex flex-row items-center gap-[5%] h-[35vh] bg-stone-400 ">
             <div className="w-fit px-5 flex flex-col">
                 <p className="font-semibold font-serif text-3xl text-orange-300 leading-loose">
                     Recommended
                 </p>
                 <span className="text-white bold text-lg">For you</span>
             </div>
-            {[1, 2, 3].map((count) => 
-                (
-                    <div className="Card1 w-[20%] h-[90%] flex items-center">
-                        {/* <div className="w-[5%] h-[100%] bg-white text-pink-sweet font-bold flex items-center flex-col justify-center">
-                            <div>T</div>
-                            <div>O</div>
-                            <div>P</div>
-                            <div>{count}</div>
-                        </div> */}
-                        {
-                            <div className="w-full h-[100%] relative flex items-center flex-col">
-                                {sortedArray && sortedArray.length > 0 && (
-                                    <Link
-                                        key={sortedArray[count].name}
-                                        href={`/hotel/${sortedArray[count].id}`}
-                                        className="w-full h-full"
-                                    >
-                                        <RecommendCard
-                                            hotelName={sortedArray[count].name}
-                                            hotelCity={sortedArray[count].city}
-                                            hotelPrice={sortedArray[count].price}
-                                            rankImage={`/img/rank/rank${count}.png`}
-                                            imgSrc={`/img/${sortedArray[count].file}`}
-                                        />
-                                    </Link>
-                                )}
-                            </div>
-                        }
-                    </div>
-                ))
-
-            }
+            <div className="w-full h-[90%]">
+                <div className="w-full h-[100%] relative flex items-center">
+                    {sortedArray && sortedArray.length > 0 ? (
+                        sortedArray.map((hotel, index) => {
+                            if(index > 2) return;
+                            let AvgReview = 0;
+                            if (hotel.review.length) {
+                                let sum: number = 0;
+                                hotel.review.forEach((item: any) => {
+                                    sum += item.stars
+                                })
+                                AvgReview = sum / hotel.review.length
+                            }
+                            return (
+                                <Link
+                                    key={hotel.name}
+                                    href={`/hotel/${hotel.id}`}
+                                    className="w-[25%] h-full mr-[5%]"
+                                >
+                                    <RecommendCard
+                                        hotelName={hotel.name}
+                                        hotelCity={hotel.city}
+                                        hotelPrice={hotel.price}
+                                        rankImage={`/img/rank/rank${index + 1}.png`}
+                                        rating={AvgReview}
+                                        count={hotel.review.length}
+                                        imgSrc={`/img/${hotel.file}`}
+                                    />
+                                </Link>
+                            )
+                        })
+                    ) : null
+                    }
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/components/RecommendCard.tsx
+++ frontend/src/components/RecommendCard.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import CardTemplate from "./CardTemplate";
 import getHotels from "@/libs/getHotels";
 import getHotel from "@/libs/getHotel";
+import { Rating } from "@mui/material";
 
-export default function RecommendCard({ hotelName, hotelCity, hotelPrice, rankImage,  imgSrc }: { hotelName: string, hotelCity: string, hotelPrice: number, rankImage: string, imgSrc: string }) {
+export default function RecommendCard({ hotelName, hotelCity, hotelPrice, rankImage, rating, count,  imgSrc }: { hotelName: string, hotelCity: string, hotelPrice: number, rankImage: string, rating: number, count: number, imgSrc: string }) {
 
     // const randPrice = Math.floor(Math.random() * (10000 - 100 + 1)) + 100;
     const formatter = new Intl.NumberFormat("th-TH", {
@@ -21,10 +22,10 @@ export default function RecommendCard({ hotelName, hotelCity, hotelPrice, rankIm
                     alt="rank image"
                 ></Image>
             </div>
-            <div className="w-[100%] h-[70%] relative rounded-xl">
+            <div className="w-[100%] h-[60%] relative rounded-xl">
                 <Image src={imgSrc} alt={hotelName} fill={true} className="object-cover rounded-t-md"/>
             </div>
-            <div className="w-full h-[30%] bg-white p-1">
+            <div className="w-full h-[40%] bg-white p-1">
                 <div className="font-bold text-lg pl-1">
                     {hotelName}
                 </div>
@@ -37,6 +38,16 @@ export default function RecommendCard({ hotelName, hotelCity, hotelPrice, rankIm
                     ></Image>
                     {hotelCity}
                 </div>
+                <div className="flex gap-1">
+                    <Rating
+                        name="read-only"
+                        value={rating}
+                        precision={0.1}
+                        readOnly
+                    />
+                    <div className="text-bold">({rating.toFixed(1)})</div>
+                </div>
+                <div className="opacity-50 pl-1">{count} reviews</div>
                 <div className="absolute bottom-0 right-0 p-1 text-2xl text-orange-500">
                     {formatter.format(hotelPrice).replace('฿', 'THB ')}.-
                 </div>
